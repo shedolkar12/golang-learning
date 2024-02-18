dcd74c93c679d134d2194185d522084f3c057be1
Rajesh Murlidhar Shedolkar <rajeshshedolkar12@gmail.com>
2024-02-19 00:08:43 +0530

    sample server program

--- concurrency.go
+++ concurrency.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func f(n int) {
 	fmt.Println(time.TimeOnly)
 	for i := 0; i < 10; i++ {
 		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250)) 
+		time.Sleep(time.Millisecond * amt) 
 	}
 }
 
@@ -17,7 +20,7 @@ func main() {
 		go f(0)
 	}
 	var input string = "rajesh"
-	// fmt.Scanln(&input)
-	time.Sleep(5)
+	fmt.Scanln(&input)
+	time.Sleep(0)
 	fmt.Println(input)
 }
